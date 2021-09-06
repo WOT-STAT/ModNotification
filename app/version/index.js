93bff2793882336fd8ae89df4a711dfd12c7efd1
Andrei <soprachev@mail.ru>
2021-09-06 03:35:12 +0300

    rename url version

--- app/version/index.js
+++ app/version/index.js
@@ -5,11 +5,11 @@ import path from 'path'
 const router = Router()
 
 
-router.get('/versions', (req, res) => {
+router.get('/version', (req, res) => {
     res.json(config.get('version'))
 })
 
-router.get('/versions/download', (req, res) => {
+router.get('/version/download', (req, res) => {
     const file = path.resolve(`../modVersions/wotStat_${config.get('version').version}.wotmod`)
     res.download(file);
 })
